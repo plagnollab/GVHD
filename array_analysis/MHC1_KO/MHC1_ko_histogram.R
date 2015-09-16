394140f580fab2e859b0127bfa66a9d088e112c7
Vincent Plagnol <vincent.plagnol@gmail.com>
2015-09-16 14:20:33 +0100

    See CHANGELOGS

--- array_analysis/MHC1_KO/MHC1_ko_histogram.R
+++ array_analysis/MHC1_KO/MHC1_ko_histogram.R
@@ -7,19 +7,19 @@ significant_TM008 = subset(TM008_no_outlier, as.vector(TM008_no_outlier$P.Value)
 non_significant_TM006 = subset(TM006_no_outlier, as.vector(TM006_no_outlier$P.Value) > 0.05)
 non_significant_TM008 = subset(TM008_no_outlier, as.vector(TM008_no_outlier$P.Value) > 0.05)
 
-write.csv(x= significant_TM006, file = paste('/cluster/project8/vyp/Winship_GVHD/claire/results/mhc1_ko/DE_D42WT_VS_D42KO_ALLANALYSIS_TM006.csv',sep =""))
-write.csv(x= non_significant_TM006, file = paste('/cluster/project8/vyp/Winship_GVHD/claire/results/mhc1_ko/non_DE_D42WT_VS_D42KO_ALLANALYSIS_TM006.csv',sep =""))
-write.csv(x= significant_TM008, file = paste('/cluster/project8/vyp/Winship_GVHD/claire/results/mhc1_ko/DE_D7WT_VS_D7KO_ALLANALYSIS_TM008.csv',sep =""))
-write.csv(x= non_significant_TM008, file = paste('/cluster/project8/vyp/Winship_GVHD/claire/results/mhc1_ko/non_DE_D7WT_VS_D7KO_ALLANALYSIS_TM008.csv',sep =""))
+write.csv(x= significant_TM006, file = paste('array_analysis/MHC1_KO/results/DE_D42WT_VS_D42KO_ALLANALYSIS_TM006.csv',sep =""))
+write.csv(x= non_significant_TM006, file = paste('array_analysis/MHC1_KO/results/non_DE_D42WT_VS_D42KO_ALLANALYSIS_TM006.csv',sep =""))
+write.csv(x= significant_TM008, file = paste('array_analysis/MHC1_KO/results/DE_D7WT_VS_D7KO_ALLANALYSIS_TM008.csv',sep =""))
+write.csv(x= non_significant_TM008, file = paste('array_analysis/MHC1_KO/results/non_DE_D7WT_VS_D7KO_ALLANALYSIS_TM008.csv',sep =""))
 
 ########### plot of p values ########
 
-png("/cluster/project8/vyp/Winship_GVHD/claire/results/mhc1_ko/D7_histogram.png")
+pdf("array_analysis/MHC1_KO/figs/D7_histogram.pdf")
 
 hist(TM008_no_outlier$P.Value, breaks=seq(0, 1, by=0.02), main = "Histogram of D7 (TM008) MHC1-KO vs WT p-values", xlab = "p-value" )
 dev.off()
 
-png("/cluster/project8/vyp/Winship_GVHD/claire/results/mhc1_ko/D42_histogram.png")
+pdf("array_analysis/MHC1_KO/figs/D42_histogram.pdf")
 TM006HIST = hist(TM006_no_outlier$P.Value, breaks=seq(0, 1, by=0.02), main = "Histogram of D42 (TM006) MHC1-KO vs WT p-values", xlab = "p-value" )
 
 dev.off()
