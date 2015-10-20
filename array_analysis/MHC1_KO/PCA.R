3e3249b5f6f83872f3d3fe9ff5ed3bae325bb10d
Vincent Plagnol <vincent.plagnol@gmail.com>
2015-10-21 00:45:22 +0100

    See CHANGELOGS

---
+++ array_analysis/MHC1_KO/PCA.R
@@ -0,0 +1,54 @@
+library(oligo)
+library(limma)
+library(lattice)
+library(smida)
+
+all.files <- list.celfiles('/cluster/project8/vyp/Winship_GVHD/claire/data_files/Teresa_microarray', full.names = TRUE, recursive = TRUE)
+
+set.probes <- 'core'  ##extended, full are other options
+affyexpression <- read.celfiles(all.files)
+summaries <- rma(affyexpression, target = set.probes)
+
+save(list = "summaries", file = "array_analysis/MHC1_KO/results/summary_core.RData")
+
+my.pca <- prcomp(t(exprs(summaries)), retx = TRUE)$x
+
+pdf("array_analysis/MHC1_KO/figs/PCA.pdf")
+plot(x = my.pca[, 1], ##first PC
+     y = my.pca[, 2])  ## second PC
+
+text(x = my.pca[, 1], ##first PC
+     y = my.pca[, 2], ##second PC
+     label = gsub(row.names(my.pca), pattern = ".CEL", replacement = ""))  ## second PC
+
+plot(x = my.pca[, 1], ##third PC
+     y = my.pca[, 2])  ## fourth PC
+
+text(x = my.pca[, 1], ##third PC
+     y = my.pca[, 2], ##fourth PC
+     label = gsub(row.names(my.pca), pattern = ".CEL", replacement = ""))  ## second PC
+
+dev.off()
+
+
+#### prettier ggplot version
+library(ggplot2)
+my.frame <- as.data.frame(my.pca[, 1:4])  ##ggplot wants data frame
+my.frame$array <- gsub(row.names(my.pca), pattern = ".CEL", replacement = "")
+
+g <- ggplot(data = my.frame, aes(x = PC1, y = PC2)) + geom_point()
+g <- g + geom_text(aes(x = PC1, y = PC2, label = array))
+ggsave(g, file = "array_analysis/MHC1_KO/figs/PCA_prettier.pdf")
+
+
+### now we now that TM008_ko4 is an outlier, it makes the graph less interesting. Let us remove and redo
+
+summaries.no.outlier <- summaries[, row.names(pData(summaries)) != "TM008_ko4.CEL"] ##here we remove TM008_ko4.CEL
+my.pca <- prcomp(t(exprs(summaries.no.outlier)), retx = TRUE)$x  ## and we redo a PCA
+my.frame <- as.data.frame(my.pca[, 1:4])  ##ggplot wants data frame
+my.frame$array <- gsub(row.names(my.pca), pattern = ".CEL", replacement = "")
+
+g <- ggplot(data = my.frame, ggplot2::aes(x = PC1, y = PC2)) + ggplot2::geom_point()
+g <- g + ggplot2::geom_text(ggplot2::aes(x = PC1, y = PC2, label = array, vjust = -1, size = 2)) + ggplot2::scale_size(guide = "none")
+g <- g + ggplot2::ggtitle("TM008_ko4.CEL removed")
+ggsave(g, file = "array_analysis/MHC1_KO/figs/PCA_prettier_outlier_removed.pdf")
